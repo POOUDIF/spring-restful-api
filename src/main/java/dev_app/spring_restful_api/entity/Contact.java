f84462b773d27c4524831ee64175502d7d2ccee2
Daffa <daffafebriansaputra101@gmail.com>
2025-06-09 11:18:02 +0700

    Contact Entity

---
+++ src/main/java/dev_app/spring_restful_api/entity/Contact.java
@@ -0,0 +1,37 @@
+package dev_app.spring_restful_api.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "contacts")
+public class Contact {
+
+    @Id
+    private String id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String phone;
+
+    private String email;
+
+    @ManyToOne
+    @JoinColumn(name = "username", referencedColumnName = "username")
+    private User user;
+
+    @OneToMany(mappedBy = "contact")
+    private List<Address> addresses;
+}
